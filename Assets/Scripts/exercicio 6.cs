45b07258ea2790c0895cd3dcf8f6f52b2debabcf
laurelzxy <laurenolivar14@gmail.com>
2024-11-05 14:40:05 -0300

    Feat: exercicio 6

---
+++ Assets/Scripts/exercicio 6.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+//O jogador pode escolher entre o
+//personagem Guerreiro ou Mago. Exiba "Guerreiro escolhido" se o
+//jogador selecionar Guerreiro e "Mago escolhido" se selecionar
+//Mago.
+public class exercicio6 : MonoBehaviour
+{
+    [SerializeField] bool guerreiro;
+    [SerializeField] bool mago;
+    void Start()
+    {
+        print(guerreiro == true ? "Guerreiro escolhido" : "Mago escolhido");
+    }
+
+    void Update()
+    {
+        
+    }
+}
