f6f1badc88d539b7bdd511b2ca322c88d00b4f63
laurelzxy <laurenolivar14@gmail.com>
2024-11-11 16:42:15 -0300

    Feat: exercicio 8

--- Assets/Scripts/estrutura de repeticao/exercicio 07.cs
+++ Assets/Scripts/estrutura de repeticao/exercicio 07.cs
@@ -6,22 +6,16 @@ using UnityEngine;
 public class exercicio07 : MonoBehaviour
 {
     //Dada uma lista de números,encontre o maior valor.
+
+    int[] maior = { 12, 20, 40, 33, 22, 10 };
     void Start()
     {
-        List<int> nums = new List<int>();
-
-        nums.Add(5);
-        nums.Add(15);
-        nums.Add(10);
-        nums.Add(22);
-        nums.Add(20);
-        nums.Add(50);
-        nums.Add(35);
-        nums.Add(36);
-
-        int n = nums.Max();
-
-        print(n);
+        for(int i = 0; i < maior.Length; i++)
+        {
+            int a = maior[i];
+            int b = maior[i];
+            
+        }
     }
 
     void Update()
---
+++ Assets/Scripts/estrutura de repeticao/exercicio 08.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+//Escreva um programa que inverte uma
+//string usando um loop for.
+public class exercicio08 : MonoBehaviour
+{
+    string nome = "laura
+    string inverter;
+    void Start()
+    {
+        for (int i =nome.Length - 1; i >= 0;  i--)
+        {
+            char world = nome[i];
+            string letra = world.ToString();
+            inverter += letra;
+        }
+        print("A palavra " + nome + " invertido é : " + inverter);
+    }
+
+    void Update()
+    {
+        
+    }
+}
