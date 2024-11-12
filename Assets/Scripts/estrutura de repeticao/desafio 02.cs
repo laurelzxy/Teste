b61287ec270a40af0dfff49318852ea777e36755
laurelzxy <laurenolivar14@gmail.com>
2024-11-12 14:53:59 -0300

    Feat: correção exercicios

---
+++ Assets/Scripts/estrutura de repeticao/desafio 02.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class desafio02 : MonoBehaviour
+{
+    //Faça um algoritmo que receba o dia, o mês e o ano em que uma pessoa nasceu, imprima na tela quantos anos,
+    //meses e dias essa pessoa já viveu. Leve em consideração o ano com 365 dias e o mês com 30 dias.
+    [SerializeField] int dia;
+
+
+
+
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/estrutura de repeticao/exercicios 01.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class exercicios01 : MonoBehaviour
+{
+    void Start()
+    {
+        //for inicializador; condição; incremento/decremento.
+        for(int i = 5; i > 0; i--)
+        {
+            print(i);
+        }
+        print("Inicio da partida!");
+    }
+
+    void Update()
+    {
+        
+    }
+}
